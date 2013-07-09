9d6a4776241c8bd27e52ef75c1d88085469bfa94
roberto <roberto@roberto-VirtualBox.(none)>
2013-07-09 19:37:14 +0200

    completato step HW3 step 1

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
+
+// var homepage = fs.readFileSync('./index.html', 'utf8');
+var buffer = fs.readFileSync('./index.html');
+  
+// console.log("stringa homepage = " + homepage);
+console.log("stringa homepage = " + buffer.toString('utf8'));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // response.send('Hello World!');
+  // response.send(homepage);
+  response.send(buffer.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
