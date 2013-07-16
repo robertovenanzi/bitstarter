44711690642babdce826776a4246cf3fd7430d43
roberto <roberto@roberto-VirtualBox.(none)>
2013-07-16 20:11:48 +0200

    HW4 step 1

--- web.js
+++ web.js
@@ -9,13 +9,15 @@ console.log("stringa homepage = " + buffer.toString('utf8'));
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
   // response.send('Hello World!');
   // response.send(homepage);
   response.send(buffer.toString('utf8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
